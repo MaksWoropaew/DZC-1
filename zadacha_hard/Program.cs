b881ea189f7e530fb638ac2b171d25c369c96bb5
M.Voropaev <maks.woropaew@mail.ru>
2022-06-26 14:46:25 +0300

    решение без проверок

---
+++ zadacha_hard/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+           // if ((number / 10 > 0) && (number / 100 ==0));
+
+            int lastnum = number % 10;
+            Console.WriteLine($"Цифра : {lastnum}");
+
+        }
+    }
+}
