e008a1ec414fde82ce16e04556affb5965a0851f
M.Voropaev <maks.woropaew@mail.ru>
2022-06-26 16:29:58 +0300

    спорная ситуация

--- zadacha_hard/Program.cs
+++ zadacha_hard/Program.cs
@@ -6,12 +6,18 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Введите число: ");
-            int number = Convert.ToInt32(Console.ReadLine());
-           // if ((number / 10 > 0) && (number / 100 ==0));
-
-            int lastnum = number % 10;
-            Console.WriteLine($"Цифра : {lastnum}");
-
+            //var variable = Console.ReadLine();
+            //var result = int.TryParse(string.Join("",variable.Where(x=>char.IsDigit(x))), out int n);
+            //Console.WriteLine(n);
+            string value = Convert.ToString(Console.ReadLine());
+            int number = Convert.ToInt32(value);
+            if (number / 100 > 0)
+            {
+                int lastnum = number % 10;
+                Console.WriteLine($"Цифра : {lastnum}");
+            }
+            else
+                Console.WriteLine("Число не трехзначное");
         }
     }
 }
